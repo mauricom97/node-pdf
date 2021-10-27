4d1d60dc2bb696bd55292e0f97c5f0646f3d25ca
Mauricio Nunes <mauricionunesdasilvanunes@gmail.com>
2021-10-27 01:39:05 -0200

    trocando resposta

--- controllers/person.js
+++ controllers/person.js
@@ -8,30 +8,30 @@ module.exports = async (req, res) => {
 
     try {
       
-      const infos = { nome: req.params.nome, idade: req.params.idade, endereco: req.params.endereco}
+    //   const infos = { nome: req.params.nome, idade: req.params.idade, endereco: req.params.endereco}
 
-        ejs.renderFile('./templates/entities.ejs', { infos: infos }, (err, html) => {
-            if(err) {
-                console.log(err)
-                return res.status(500).json({message: 'error in server'})
-            }
+    //     ejs.renderFile('./templates/entities.ejs', { infos: infos }, (err, html) => {
+    //         if(err) {
+    //             console.log(err)
+    //             return res.status(500).json({message: 'error in server'})
+    //         }
         
-            const options = {
-                format: 'A4',
-                orientation: 'portrait'
+    //         const options = {
+    //             format: 'A4',
+    //             orientation: 'portrait'
 
-            }
+    //         }
         
-            pdf.create(html, options).toFile('uploads/reports.pdf', (error, response) => {
-                if(!error){
-                    return res.json({message: 'PDF Generate'})
-                }else{
-                    console.log(error)
-                    return res.json({message: 'Fail in Genarated PDF'})
-                }
-            })
-        })
-
+    //         pdf.create(html, options).toFile('uploads/reports.pdf', (error, response) => {
+    //             if(!error){
+    //                 return res.json({message: 'PDF Generate'})
+    //             }else{
+    //                 console.log(error)
+    //                 return res.json({message: 'Fail in Genarated PDF'})
+    //             }
+    //         })
+    //     })
+res.send({"Mensagem": "Rinha de galo"})
     } catch (error) {
         
      console.log(error)
